3296947ff024fd58f8cf7330dea744f2eacba084
Godwin Banda <godwinbanda19@gmail.com>
2021-08-04 00:33:15 +0200

    edit

--- website/settings.py
+++ website/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.43.119']
+ALLOWED_HOSTS = ['zedbuzzers.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 
