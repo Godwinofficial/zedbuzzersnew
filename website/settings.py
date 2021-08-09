6fc0b93bc7736a2c3d2a26716b4668f61e20cebe
Godwin Banda <godwinbanda19@gmail.com>
2021-08-09 15:13:35 +0200

    edit

--- website/settings.py
+++ website/settings.py
@@ -36,9 +36,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b(fpsx=ea^-+#^6gk=cz*p^co0-q@o^(fjs*hzhm-%sbm$ygrb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 SECURE_BROWSER_XSS_FILTER = True
